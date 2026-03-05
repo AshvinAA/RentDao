8ec0e2b042aa7b1e70340cec77756a97768c4829
AshvinAA <ashvin.afroz@gmail.com>
2026-03-05 23:29:17 +0600

    Assignment03_370

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")  #creates a Jinja2 object and tells it to look to the templates package for everything
 
 @app.get("/")
-def read_root(request: Request):#request is a variable that basically catches all our data and packs them into our request variable
+def read_root(request: sRequest):#request is a variable that basically catches all our data and packs them into our request variable
     # format for templates=         inside index html we would perform a request and the message that would be passed is "message"
     return templates.TemplateResponse("index.html" , {"request": request , "message": "Welcomes to RentDao's new frontend"})
+
+
+
+
+
+
 
 
 #uvicorn main:app --reload

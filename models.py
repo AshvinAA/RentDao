4d059cecfd01d06d22d8d11456624efb71453f13
AshvinAA <ashvin.afroz@gmail.com>
2026-04-22 16:06:30 +0600

    Add booking details model and enhance user and item schemas

--- models.py
+++ models.py
@@ -15,6 +15,10 @@ class Item(Base):
     # rating=Column(Float, default=0.0)
     owner_id = Column(Integer, ForeignKey("users.id"))
     owner = relationship("User", back_populates="items_posted")
+    price_per_day = Column(Integer , index = True)
+    discount = Column(Integer , default=0)
+    rating =Column(Float , index = True)
+
 
 class User(Base):
     __tablename__= "users"
@@ -27,7 +31,7 @@ class User(Base):
 
     #Profile Details
     picture = Column(String(255) , nullable =True) #We will store the image URL/path and not the image file itself
-    location = Column(String(100))
+    location = Column(String(100), index=True, nullable=False)
     phone_no = Column(String(20))
     rating = Column(Float, default=0.0)
     payment_option = Column(String(100))
@@ -35,11 +39,33 @@ class User(Base):
     #Admin/Suspended Flags
     is_admin = Column(Boolean , default=False)
     is_suspended = Column(Boolean , default=False)
+    user_rating = Column(Float,index=True, default=0.0)
+    items_count = Column(Integer, index=True, default=0)
+
 
     items_posted = relationship("Item",back_populates="owner")
 
-    #Uncomment when rental is done
-    #history =relationship("Rental",back_populates="user")
+    history = relationship("Booking_details", backref="user")
+    
+class Booking_details(Base):
+    __tablename__="booking_details"
+
+    id=Column(Integer, primary_key=True, index=True)
+    item_id=Column(Integer, ForeignKey("items.id"))
+    user_id=Column(Integer, ForeignKey("users.id"))
+    item = relationship("Item")
+    start_date=Column(String(20))
+    end_date=Column(String(20))
+    total_price=Column(Integer)
+    status = Column(String(20), default="pending") #pending, approved, rejected, completed
+    booking_status = Column(Boolean, default=False) #False means the booking is not yet completed, True means the booking is completed
+    rentor_id = Column(Integer, ForeignKey("users.id") ,default=None) 
+    @property
+    def location(self):
+        if self.item and self.item.owner:
+            return self.item.owner.location
+        return None
+    
     
 
 
